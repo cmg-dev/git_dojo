c95d4d492029ee4e852c4c4480cd1ea5c7735675
Christoph Gnip <christoph.gnip@5minds.de>
2017-04-26 18:04:28 +0200

    Squashed stuff
    **Changes**
    
    * Add new file
    * Add demo Python File with Class
    * Add Function Test1
    * Add Constructor Variables
    * Add return Value

---
+++ test.py
@@ -0,0 +1,19 @@
+class DemoClass(object):
+
+    """Docstring for DemoClass. """
+
+    def __init__(self):
+        self.var1="test"
+        self.var2="bla"
+
+        """TODO: to be defined1. """
+
+    def Test1(self, arg1):
+        """TODO: Docstring for Test1.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        return true
+
